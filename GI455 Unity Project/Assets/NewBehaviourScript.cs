f95e09046ac484f8ea2eb1aa3519f9729f2c191a
LAPTOP-N449OIKN\weerawut <weerawut.boon@bumail.net>
2021-01-29 21:40:15 +0700

    SaveSave

---
+++ GI455 Unity Project/Assets/NewBehaviourScript.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    public string inputword;
+    public GameObject inputto;
+    public GameObject Field;
+
+    //Data 
+    public string[] dataentry;
+    //public string[] Dataentry = {"Nivea" , "YaKult" , "Glade" , "Water" , "Vivo" , "Candy" , "Hdmi" , "Spin"}
+
+    public void dataroom()
+    {
+        inputword = inputto.GetComponent<InputField>().text;
+        if (dataentry.Contains(inputword)) 
+        {
+            inputto.GetComponent<InputField>().text =  "<color=#00328A>" + inputword + "</color> Is Found";
+            
+        }
+        else
+        {
+            inputto.GetComponent<InputField>().text =   "<color=#8A0000>" + inputword + "</color> Is Not Found" ;
+        }
+
+
+
+    }
+}
+ 
